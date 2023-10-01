2cb172f4b8e2fe403f3d376bc9cf4069798961b3
Chester <chester44311@gmail.com>
2023-10-01 14:38:08 +0800

    Adding logout button

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,12 +1,33 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import React from "react";
 
-const TopAppBar = () => {
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const TopAppBar = (props) => {
+    const { name } = props;
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch({
+            type: "auth/logoutUser"
+        });
+        navigate("/login")
+    }
+
     return (
         <>
             <AppBar title="Login Application" position="static">
                 <Toolbar title="Login">
-                
+                    {name !== "login" ? (
+                        <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "flex-end"}}>
+                            <Button color="inherit" variant="outlined" onClick={handleLogout}>Logout</Button>
+                        </Box>
+                    ) : (
+                        ""
+                    )}
                 </Toolbar>
             </AppBar>
         </>
