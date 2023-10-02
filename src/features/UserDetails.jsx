a3fe1e790c3f2d6e6c9b09ce631d1cc43286ce72
Chester <chester44311@gmail.com>
2023-10-02 21:06:21 +0800

    User details
    Updating styling of user details to use Grid

--- src/features/UserDetails.jsx
+++ src/features/UserDetails.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, Typography, Container } from "@mui/material";
+import { Box, Card, Typography, Container, Grid } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -23,53 +23,65 @@ const UserDetails = (props) => {
                 }}
             >
                 <Card style={{ width: "100%" }}>
-                    <Box>
+                    <Box sx={{display: "flex"}}>
                         {permissionList.includes("VL01") ? (
-                            <Box p={2}>
-                                <Typography
-                                    variant="h4"
-                                    sx={{ fontWeight: "bold" }}
-                                >
-                                    Welcome {userInfo.displayName}
-                                </Typography>
-                                <Typography
-                                    variant="h5"
-                                    sx={{ fontSize: "12pt" }}
-                                >
-                                    UserName : {userInfo.username}
-                                </Typography>
-                                <Typography
-                                    variant="h5"
-                                    sx={{ fontSize: "12pt" }}
-                                >
-                                    Role: {userInfo.role}
-                                </Typography>
-                            </Box>
+                            <Grid container spacing={3} sx={{ px: 1, py: 1 }}>
+                                {/* Header : Welcome text */}
+                                <Grid item xs={12}>
+                                    <Typography
+                                        variant="h4"
+                                        sx={{ fontWeight: "bold" }}
+                                    >
+                                        Welcome {userInfo.displayName}
+                                    </Typography>
+                                </Grid>
+
+                                {/* User details */}
+                                <Grid item xs={12}>
+                                    <Grid
+                                        container
+                                        alignItems={"center"}
+                                        spacing={2}
+                                    >
+                                        <Grid item xs={8}>
+                                            <Typography
+                                                variant="h5"
+                                                sx={{ fontSize: "12pt" }}
+                                            >
+                                                <b>UserName</b>:{" "}
+                                                {userInfo.username}
+                                            </Typography>
+                                            <Typography
+                                                variant="h5"
+                                                sx={{ fontSize: "12pt" }}
+                                            >
+                                                <b>Role</b>: {userInfo.role}
+                                            </Typography>
+                                        </Grid>
+
+                                        {/* View all account: only seen by manager role */}
+                                        {permissionList.includes("VL02") ? (
+                                            <Grid item xs={4} md={6}>
+                                                <Link to="/allAccountPage">
+                                                    <Typography
+                                                        variant="h5"
+                                                        sx={{
+                                                            fontWeight: "bold",
+                                                            fontSize: "12pt",
+                                                        }}
+                                                    >
+                                                        View all accounts
+                                                    </Typography>
+                                                </Link>
+                                            </Grid>
+                                        ) : (
+                                            ""
+                                        )}
+                                    </Grid>
+                                </Grid>
+                            </Grid>
                         ) : (
                             "You are not authorised to view this information"
-                        )}
-                        {permissionList.includes("VL02") ? (
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    justifyContent: "flex-end",
-                                }}
-                            >
-                                <Link to="/allAccountPage">
-                                    <Typography
-                                        variant="h5"
-                                        sx={{
-                                            fontWeight: "bold",
-                                            fontSize: "12pt",
-                                            px: 1,
-                                        }}
-                                    >
-                                        View all accounts
-                                    </Typography>
-                                </Link>
-                            </Box>
-                        ) : (
-                            ""
                         )}
                     </Box>
                 </Card>
